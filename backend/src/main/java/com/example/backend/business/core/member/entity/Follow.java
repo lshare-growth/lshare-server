fd677bf2e7a92f61a3dc5367c94ffb3b8b8d2050
devjun10 <devjun1023@gmail.com>
2022-11-02 02:26:27 +0900

    refactor: 팔로잉 엔티티 lastModifiedAt 컬럼 제거
    - 사용하지 않는 불필요한 컬럼 제거

--- backend/src/main/java/com/example/backend/business/core/member/entity/Follow.java
+++ backend/src/main/java/com/example/backend/business/core/member/entity/Follow.java
@@ -1,6 +1,6 @@
 package com.example.backend.business.core.member.entity;
 
-import com.example.backend.business.core.common.values.DateTime;
+import com.example.backend.business.core.common.values.CreatedAt;
 import com.fasterxml.uuid.Generators;
 
 import javax.persistence.Column;
@@ -28,7 +28,7 @@ public class Follow {
     private Member target;
 
     @Embedded
-    private DateTime dateTime;
+    private CreatedAt createdAt;
 
     /**
      * @Nullary-Constructor. JPA 기본 생성자로 member 외부 패키지에서 호출하지 말 것.
@@ -41,7 +41,7 @@ public class Follow {
         this.UUID = createUUID();
         this.source = source;
         this.target = target;
-        this.dateTime = DateTime.initDateTime();
+        this.createdAt = CreatedAt.initCreatedAt();
     }
 
     private void validateFollow(Member source, Member target) {
@@ -66,8 +66,8 @@ public class Follow {
         return target;
     }
 
-    public DateTime getDateTime() {
-        return dateTime;
+    public CreatedAt getCreatedAt() {
+        return createdAt;
     }
 
     public boolean isFollowing(Member target) {
@@ -87,8 +87,8 @@ public class Follow {
         return Objects.hash(getSource(), getTarget());
     }
 
-//    @Override
-//    public String toString() {
-//        return String.format("팔로우 주체: %s, 팔로잉 대상: %s", source, target);
-//    }
+    @Override
+    public String toString() {
+        return UUID.toString();
+    }
 }
